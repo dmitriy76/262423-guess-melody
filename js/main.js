264f11997b3908fd5d2175294c76d2bd521d496e
Polina <p.katysheva@gmail.com>
2018-04-05 16:18:39 +0300

    modules created

--- js/main.js
+++ js/main.js
@@ -1,38 +1,5 @@
-const appScreen = document.querySelector(`.app .main`);
-const templates = document.querySelector(`#templates`).content;
+import {showTemplate} from './utils';
+import welcome from './screens/welcomeScreen';
 
-let welcomeScreen = templates.querySelector(`.main--welcome`);
-let artistScreen = templates.querySelector(`.main--level-artist`);
-let genreScreen = templates.querySelector(`.main--level-genre`);
-let resultScreens = templates.querySelectorAll(`.main--result`);
+showTemplate(welcome);
 
-let orderedScreens = [welcomeScreen, artistScreen, genreScreen].concat([...resultScreens]);
-
-const LEFT_ARROW_KEY = 37;
-const RIGHT_ARROW_KEY = 39;
-
-let currentWindowNum = 0;
-
-const showScreen = function (number) {
-  if (appScreen.children[0]) {
-    appScreen.removeChild(appScreen.children[0]);
-  }
-  appScreen.appendChild(orderedScreens[number]);
-};
-
-const changeScreen = function (event) {
-  event.preventDefault();
-
-  if (event.altKey && event.keyCode === LEFT_ARROW_KEY) {
-    currentWindowNum = currentWindowNum > 0 ? (currentWindowNum - 1) : 0;
-    showScreen(currentWindowNum);
-  }
-  if (event.altKey && event.keyCode === RIGHT_ARROW_KEY) {
-    currentWindowNum = currentWindowNum < orderedScreens.length - 1 ? (currentWindowNum + 1) : orderedScreens.length - 1;
-    showScreen(currentWindowNum);
-  }
-};
-
-showScreen(currentWindowNum);
-
-document.addEventListener(`keydown`, changeScreen);
